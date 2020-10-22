d0b8ae2a07593248296a421946ebceb8cb40aa9d
maxie <maxiperezc@gmail.com>
2020-10-22 19:30:04 -0300

    accel example

---
+++ app/src/main/java/com/example/memoscopio/BubbleView.java
@@ -0,0 +1,63 @@
+package com.example.memoscopio;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+
+public class BubbleView extends View {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int diameter = 100;
+
+    private int maxLeft;
+    private int maxTop;
+    private int maxRight;
+    private int maxBottom;
+
+    private ShapeDrawable bubble;
+
+    public BubbleView(Context context, int width, int height)
+    {
+        super(context);
+
+        this.width = width;
+        this.height = height;
+        this.x = (width-diameter)/2;
+        this.y = (height/2) - diameter;
+
+        this.maxLeft = diameter;
+        this.maxTop = diameter;
+        this.maxRight = width - diameter;
+        this.maxBottom = height - diameter;
+
+        bubble = new ShapeDrawable(new OvalShape());
+        bubble.setBounds(x, y, x + diameter, y + diameter);
+        bubble.getPaint().setColor(Color.BLUE);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        bubble.draw(canvas);
+    }
+
+    protected void move(float f, float g) {
+        if(x < maxLeft || x > maxRight || y < maxTop || y > maxBottom) return;
+
+        x = (int) (x + f);
+        y = (int) (y + g);
+        bubble.setBounds(x, y, x + diameter, y + diameter);
+        invalidate();
+    }
+}
--- app/src/main/java/com/example/memoscopio/GameActivity.java
+++ app/src/main/java/com/example/memoscopio/GameActivity.java
@@ -7,118 +7,43 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.util.DisplayMetrics;
 
-public class GameActivity extends AppCompatActivity implements SensorEventListener  {
+public class GameActivity extends AppCompatActivity implements SensorEventListener {
 
-    private SensorManager sensorManager;
-
-    private TextView acelerometro;
-    private TextView giroscopio;
-    private TextView proximidad;
-    private TextView detecta;
-    private TextView presion;
-
+    private SensorManager manager;
+    private BubbleView bubbleView;
+    private Sensor accel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
 
-        acelerometro = findViewById(R.id.acelerometro);
-        giroscopio = findViewById(R.id.giroscopio);
-        proximidad = findViewById(R.id.proximidad);
-        detecta = findViewById(R.id.detecta);
-        presion = findViewById(R.id.presion);
+        bubbleView = new BubbleView(GameActivity.this, width, height);
+        setContentView(bubbleView);
+
+        manager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accel = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        manager.registerListener(GameActivity.this, accel, SensorManager.SENSOR_DELAY_GAME);
     }
 
-    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
     public void onSensorChanged(SensorEvent event) {
-        String txt = "";
-
-        synchronized (this) {
-
-            Log.d("sensor", event.sensor.getName());
-            Log.d("sensor 2 id", String.valueOf(this.getTaskId()));
-
-            switch (event.sensor.getType()){
-                case Sensor.TYPE_ACCELEROMETER:
-                    txt += "acelerometro:\n";
-                    txt += "x: " + event.values[0] + "\n";
-                    txt += "y: " + event.values[1] + "\n";
-                    txt += "z: " + event.values[2] + "\n";
-                    acelerometro.setText(txt);
-
-                    if(event.values[0] > 25 || event.values[1] > 25 || event.values[2] > 25){
-                        Toast.makeText(GameActivity.this, "shake that ass baby", Toast.LENGTH_LONG).show();
-                    }
-                    break;
-                case Sensor.TYPE_GYROSCOPE:
-                    txt += "giroscopio:\n";
-                    txt += "x: " + event.values[0] + "\n";
-                    txt += "y: " + event.values[1] + "\n";
-                    txt += "z: " + event.values[2] + "\n";
-                    giroscopio.setText(txt);
-                    break;
-                case Sensor.TYPE_PROXIMITY:
-                    txt += "proximidad:\n";
-                    txt += event.values[0] + "\n";
-                    proximidad.setText(txt);
-
-                    if(event.values[0] == 0){
-                        detecta.setText("proximidad detectada");
-                    } else {
-                        detecta.setText("-");
-                    }
-                    break;
-                case Sensor.TYPE_PRESSURE:
-                    txt += "presion:\n";
-                    txt += event.values[0] + "\n";
-                    presion.setText(txt);
-                    break;
-            }
-        }
+        bubbleView.move(event.values[0], event.values[1]);
     }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        stopSensors();
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        startSensors();
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
-        startSensors();
+        manager.registerListener(GameActivity.this, accel, SensorManager.SENSOR_DELAY_GAME);
     }
-
-    protected void startSensors(){
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_FASTEST);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_FASTEST);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE), SensorManager.SENSOR_DELAY_FASTEST);
+    protected void onPause() {
+        super.onPause();
+        manager.unregisterListener(this);
     }
-
-    protected void stopSensors(){
-        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
-        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE));
-        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY));
-        sensorManager.unregisterListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE));
-    }
-
 }
