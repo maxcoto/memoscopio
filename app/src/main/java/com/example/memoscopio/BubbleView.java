4c90921b142b5ddb4a458d93abeed13255f4f9d7
maxie <maxiperezc@gmail.com>
2020-10-23 08:02:22 -0300

    working on device

--- app/src/main/java/com/example/memoscopio/BubbleView.java
+++ app/src/main/java/com/example/memoscopio/BubbleView.java
@@ -8,10 +8,14 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
 import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class BubbleView extends View {
 
@@ -19,14 +23,19 @@ public class BubbleView extends View {
     private int y;
     private int width;
     private int height;
-    private int diameter = 100;
+    private int diameter = 50;
+    private double speed = 2.0;
 
     private int maxLeft;
     private int maxTop;
     private int maxRight;
     private int maxBottom;
 
+    private boolean starting = true;
+
     private ShapeDrawable bubble;
+
+    private ArrayList<ShapeDrawable> rects;
 
     public BubbleView(Context context, int width, int height)
     {
@@ -37,8 +46,8 @@ public class BubbleView extends View {
         this.x = (width-diameter)/2;
         this.y = (height/2) - diameter;
 
-        this.maxLeft = diameter;
-        this.maxTop = diameter;
+        this.maxLeft = 0;
+        this.maxTop = 0;
         this.maxRight = width - diameter;
         this.maxBottom = height - diameter;
 
@@ -53,13 +62,14 @@ public class BubbleView extends View {
     }
 
     protected void move(float f, float g) {
-        if(x < maxLeft && f < 0) return;
-        if(x > maxRight && f > 0) return;
+        //if(starting) return;
+        if(x < maxLeft && f > 0) return;
+        if(x > maxRight && f < 0) return;
         if(y < maxTop && g < 0) return;
         if(y > maxBottom && g > 0) return;
 
-        x = (int) (x + f);
-        y = (int) (y + g);
+        x = (int) (x - f * speed);
+        y = (int) (y + g * speed);
         bubble.setBounds(x, y, x + diameter, y + diameter);
         invalidate();
     }
